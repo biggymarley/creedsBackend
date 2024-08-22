09cd5e191d07a708e7033e425574fe6773a9e7a4
afaragi <afaragi@zobristinc.com>
2024-08-22 15:32:43 +0100

    cron

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cron = require('node-cron');
+const { default: axios } = require("axios");
 const corsOptions = {
   origin: process.env.ORIGIN,
 };
