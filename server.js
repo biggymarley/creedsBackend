2cb84ba279fb580b86cc6b18cffbfad4ce281e09
afaragi <afaragi@zobristinc.com>
2024-09-17 15:42:59 +0100

    cron

--- server.js
+++ server.js
@@ -104,16 +104,16 @@ app.post("/api/create-promo", async (req, res) => {
   }
 });
 
-cron.schedule("* * * * *", async () => {
-  console.log("Running daily check for subscriptions with points");
+// cron.schedule("* * * * *", async () => {
+//   console.log("Running daily check for subscriptions with points");
 
-  // Fetch users with active subscriptions and points from your database
-  const users = await getUsersWithActiveSubscriptionsAndPoints(stripe);
-  console.log(users);
-  for (const user of users) {
-    await applyDiscountBasedOnPoints(user, stripe);
-  }
-});
+//   // Fetch users with active subscriptions and points from your database
+//   const users = await getUsersWithActiveSubscriptionsAndPoints(stripe);
+//   console.log(users);
+//   for (const user of users) {
+//     await applyDiscountBasedOnPoints(user, stripe);
+//   }
+// });
 app.listen(5252, () =>
   console.log(`Node server listening at http://localhost:5252`)
 );
